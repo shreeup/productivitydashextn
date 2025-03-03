1ec694817743c48061fb3803302170cec1126738
Shree <fnushree@lucidmotors.com>
2025-03-03 10:35:51 -0800

    delete all files

--- apps/frontend/src/types/Task.ts
+++
@@ -1,8 +0,0 @@
-export type Task = {
-  id: string;
-  title: string;
-  description: string;
-  category: string; // e.g., work, personal, urgent
-  duedate: string;
-  priority: 'low' | 'medium' | 'high';
-};
--- apps/frontend/src/utils/localStorage.ts
+++
@@ -1,8 +0,0 @@
-export function saveTasksToLocalStorage(tasks: any) {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-}
-
-export function getTasksFromLocalStorage(): any[] {
-  const tasks = localStorage.getItem('tasks');
-  return tasks ? JSON.parse(tasks) : [];
-}
